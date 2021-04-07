abb803539a28567a5046a79839f3b432e0d77bc5
reireias <reireias@gmail.com>
2021-04-08 01:46:13 +0900

    Fix rds_cluster final_snapshot_identifier

--- rds.tf
+++ rds.tf
@@ -1,19 +1,20 @@
 resource "aws_rds_cluster" "main" {
-  cluster_identifier      = "${local.name}-cluster"
-  engine                  = "aurora-postgresql"
-  engine_version          = "12.4"
-  availability_zones      = local.availability_zones
-  database_name           = "mydb"
-  master_username         = "myuser"
-  master_password         = data.aws_kms_secrets.secrets.plaintext["db_password"]
-  backup_retention_period = 14
-  preferred_backup_window = "18:00-19:00"
-  storage_encrypted       = true
-  kms_key_id              = aws_kms_key.rds.arn
-  db_subnet_group_name    = aws_db_subnet_group.main.id
-  vpc_security_group_ids  = [aws_security_group.rds.id]
-  deletion_protection     = true
-  skip_final_snapshot     = false
+  cluster_identifier        = "${local.name}-cluster"
+  engine                    = "aurora-postgresql"
+  engine_version            = "12.4"
+  availability_zones        = local.availability_zones
+  database_name             = "mydb"
+  master_username           = "myuser"
+  master_password           = data.aws_kms_secrets.secrets.plaintext["db_password"]
+  backup_retention_period   = 14
+  preferred_backup_window   = "18:00-19:00"
+  storage_encrypted         = true
+  kms_key_id                = aws_kms_key.rds.arn
+  db_subnet_group_name      = aws_db_subnet_group.main.id
+  vpc_security_group_ids    = [aws_security_group.rds.id]
+  deletion_protection       = true
+  skip_final_snapshot       = false
+  final_snapshot_identifier = "${local.name}-cluster-final"
 
   db_cluster_parameter_group_name = aws_rds_cluster_parameter_group.main.id
 }
